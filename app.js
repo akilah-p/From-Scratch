e5e0fda79ad5a76b4b09ccc73a85e489083772da
Akilah Powell <atpjun@gmail.com>
2022-11-01 14:11:22 -0400

    changes

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 /* Imports */
-const img1 = document.getElementById('img-1');
+const img1 = document.querySelector('#img-1');
 const img2 = document.querySelector('#img-2');
 const img3 = document.querySelector('#img-3');
 
 const button1 = document.getElementById('button-1');
-const button2 = document.getElementById('button-2');
-const button3 = document.getElementById('button-3');
+
 
 
 const winsEl = document.getElementById('wins');
@@ -15,6 +14,8 @@ const totalEl = document.getElementById('total');
 /* State */
 let wins = 0;
 let total = 0;
+
+
 
 /* Events */
 button1.addEventListener('click', () => {
@@ -33,37 +34,7 @@ button1.addEventListener('click', () => {
     displayResults();
 });
 
-button2.addEventListener('click', () => {
-    total++;
-    resetShells();
-    const ballLocation = Math.ceil(Math.random() * 3);
-    if (ballLocation === 1) {
-        wins++;
-        img1.classList.add('reveal');
-        
-    } else if (ballLocation === 2) {
-        img2.classList.add('reveal');
-    } else {
-        img3.classList.add('reveal');
-    }
-    displayResults();
-});
 
-button3.addEventListener('click', () => {
-    total++;
-    resetShells();
-    const ballLocation = Math.ceil(Math.random() * 3);
-    if (ballLocation === 1) {
-        wins++;
-        img1.classList.add('reveal');
-        
-    } else if (ballLocation === 2) {
-        img2.classList.add('reveal');
-    } else {
-        img3.classList.add('reveal');
-    }
-    displayResults();
-});
 
 /* Display Functions */
 function displayResults() {
