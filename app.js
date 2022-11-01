0e1bb0c97947d734ab5b397cee62a0b114e21cf1
Akilah Powell <atpjun@gmail.com>
2022-10-31 20:40:05 -0400

    html and jss changes

--- app.js
+++ app.js
@@ -1,81 +1,81 @@
 /* Imports */
+const img1 = document.getElementById('img-1');
+const img2 = document.querySelector('#img-2');
+const img3 = document.querySelector('#img-3');\
 
-/* Get DOM Elements */
-const ball1 = document.getElementById('ball-1');
-const ball2 = document.getElementById('ball-2');
-const ball3 = document.getElementById('ball-3');
+const button1 = document.getElementById('button-1');
+const button2 = document.getElementById('button-2');
+const button3 = document.getElementById('button-3');
 
-const button1 = document.getElementById('btn1');
-const button2 = document.getElementById('btn2');
-const button3 = document.getElementById('btn3');
 
-const winsSpan = document.getElementById('wins');
-const lossesSpan = document.getElementById('losses');
-const totalSpan = document.getElementById('total');
-
-let wins = 0;
-let total = 0;
-
-function resetBalls () {
-    ball1.classList.remove('reveal');
-    ball2.classList.remove('reveal');
-    ball3.classList.remove('remove');
-}
-
-function displayResults(){
-    winsSpan.textContent = wins;
-    totalSpan.textContent = total;
-    lossesSpan.textContent = total - wins;
-}
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
 
 /* State */
+let wins =0;
+let total = 0;
 
 /* Events */
 button1.addEventListener('click', () => {
     total++;
-    resetBalls();
+    resetShells();
     const ballLocation = Math.ceil(Math.random()* 3);
     if (ballLocation === 1) {
-        ball1.classList.add('reveal');
         wins++;
+        img1.classList.add('reveal');
+        
     } else if (ballLocation === 2) {
-        ball2.classList.add('reveal');
+        img2.classList.add('reveal');
     } else {
-        ball3.classList.add('reveal');
+        img3.classList.add('reveal');
     }
     displayResults();
 });
 
 button2.addEventListener('click', () => {
     total++;
-    resetBalls();
+    resetShells();
     const ballLocation = Math.ceil(Math.random()* 3);
     if (ballLocation === 1) {
-        ball1.classList.add('reveal');
         wins++;
+        img1.classList.add('reveal');
+        
     } else if (ballLocation === 2) {
-        ball2.classList.add('reveal');
+        img2.classList.add('reveal');
     } else {
-        ball3.classList.add('reveal');
+        img3.classList.add('reveal');
     }
     displayResults();
 });
 
 button3.addEventListener('click', () => {
     total++;
-    resetBalls();
+    resetShells();
     const ballLocation = Math.ceil(Math.random()* 3);
     if (ballLocation === 1) {
-        ball1.classList.add('reveal');
         wins++;
+        img1.classList.add('reveal');
+        
     } else if (ballLocation === 2) {
-        ball2.classList.add('reveal');
+        img2.classList.add('reveal');
     } else {
-        ball3.classList.add('reveal');
+        img3.classList.add('reveal');
     }
     displayResults();
 });
 
 /* Display Functions */
+function displayResults() {
+    winsEl.textContent = wins;
+    lossesEl.textContent = total - wins;
+    total.textContent = total;
+}
+
+function resetShells() {
+    img1.classList.remove('reveal');
+    img2.classList.remove('reveal');
+    img3.classList.remove('reveal');
+}
 
 // (don't forget to call any display functions you want to run on page load!)
