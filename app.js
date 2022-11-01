afb91d8f81176d50926bba4c6449922e15f10f28
Akilah Powell <atpjun@gmail.com>
2022-11-01 16:08:24 -0400

    work in progress

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const img2 = document.querySelector('#img-2');
 const img3 = document.querySelector('#img-3');
 
 const button1 = document.getElementById('button-1');
-
+const button2 = document.getElementById('button-2');
+const button3 = document.getElementById('button-3');
 
 
 const winsEl = document.getElementById('wins');
@@ -34,19 +35,57 @@ button1.addEventListener('click', () => {
     displayResults();
 });
 
+button2.addEventListener('click', () => {
+    total++;
+    resetShells();
+    const ballLocation = Math.ceil(Math.random() * 3);
+    if (ballLocation === 1) {
+       
+        img1.classList.add('reveal');
+        
+    } else if (ballLocation === 2) {
+        img2.classList.add('reveal');
+        wins++;
+    } else {
+        img3.classList.add('reveal');
+    }
+    displayResults();
+});
+
+button3.addEventListener('click', () => {
+    total++;
+    resetShells();
+    const ballLocation = Math.ceil(Math.random() * 3);
+    if (ballLocation === 1) {
+       
+        img1.classList.add('reveal');
+        
+    } else if (ballLocation === 2) {
+        img2.classList.add('reveal');
+    } else {
+        img3.classList.add('reveal');
+        wins++;
+    }
+    displayResults();
+});
+
+
 
 
 /* Display Functions */
-function displayResults() {
-    winsEl.textContent = wins;
-    lossesEl.textContent = total - wins;
-    total.textContent = total;
-}
-
 function resetShells() {
     img1.classList.remove('reveal');
     img2.classList.remove('reveal');
     img3.classList.remove('reveal');
 }
+
+function displayResults() {
+    winsEl.textContent = wins;
+    lossesEl.textContent = total - wins;
+    totalEl.textContent = total;
+}
+
+
+
 
 // (don't forget to call any display functions you want to run on page load!)
